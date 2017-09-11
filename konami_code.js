7bdf502a14dc763904a1219238e12d02e92cde36
David Kuruvilla <dapawn@gmail.com>
2017-09-11 19:09:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+
+  // Keep track of index outside of the event handler.
+  let index = 0;
+
+  document.addEventListener('keydown', KeyDownHandler(event) )
+
+  // This is the function that would be invoked by the event listener.
+  function KeyDownHandler(event) {
+    const key = parseInt(event.detail || event.location || event.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0
+      }
+    } else {
+      index = 0;
+    }
+  }
+
 
 }
