774767a5a6e728adcfb56b026596d2eb46f7e871
David Kuruvilla <dapawn@gmail.com>
2017-09-11 20:12:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,11 +5,11 @@ function init() {
   // Keep track of index outside of the event handler.
   let index = 0;
 
-  window.addEventListener('keydown', KeyDownHandler(event) )
+  window.addEventListener('keydown', KeyDownHandler(e) )
 
   // This is the function that would be invoked by the event listener.
-  function KeyDownHandler(event) {
-    const key = parseInt(event.detail || event.location || event.which);
+  function KeyDownHandler(e) {
+    const key = parseInt(e.detail || e.location || e.which);
 
     if (key === code[index]) {
       index++;
